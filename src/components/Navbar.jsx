55ffa89709d28ca77fde5cc8add06aeb8a0fa84a
EhsaN Karimi <ehsan.karimmi@gmail.com>
2023-06-22 01:34:34 +0330

    Complete navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,45 @@
 import { useState } from "react";
-import { BiWorld } from "react-icons/bi";
+import worldIcon from "../assets/world.svg";
+import { Link } from "react-router-dom";
+import { RiSunFill, RiMoonClearFill } from "react-icons/ri";
 
 function Navbar() {
-    const [mode, setMode] = useState(true);
+    const [darkMode, setDarkMode] = useState(false);
 
+    // Change state and add 'dark:' class.
     const toggleDarkMode = () => {
-        if (mode) {
+        if (!darkMode) {
             document.documentElement.classList.add("dark");
-            setMode(!mode);
+            setDarkMode(!darkMode);
         } else {
             document.documentElement.classList.remove("dark");
-            setMode(!mode);
+            setDarkMode(!darkMode);
         }
     };
 
     return (
-        <div className="bg-gray-500 flex items-center justify-between py-3 px-5 shadow-md rounded-md ">
-            <div className="flex justify-center items-center gap-2">
-                <BiWorld className="text-4xl" />
-                <p className="uppercase">Country Searcher</p>
-            </div>
-            <button onClick={() => toggleDarkMode()}>{mode ? "Dark" : "Light"}</button>
+        // Navbar Container.
+        <div className="bg-[#42C2FF] flex items-center justify-between py-3 px-5 shadow-md rounded-md ">
+            {/* Title and Icon */}
+            <Link to="/">
+                <div className="flex justify-center items-center gap-2  cursor-pointer">
+                    <img src={worldIcon} alt="World Icon" className="w-10" />
+                    <p className="uppercase text-lg font-semibold text-gray-800">
+                        Country Searcher
+                    </p>
+                </div>
+            </Link>
+            {/* DarkMode Toggler */}
+            <button
+                onClick={() => toggleDarkMode()}
+                className="text-2xl bg-blue-200 p-2 rounded-full"
+            >
+                {darkMode ? (
+                    <RiSunFill className="text-yellow-500" />
+                ) : (
+                    <RiMoonClearFill className="text-gray-500" />
+                )}
+            </button>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { BiWorld } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import CountryCard from "../components/CountryCard";
 
 function Home() {
     const [countries, setCountries] = useState([]);
@@ -13,26 +14,26 @@ function Home() {
 
     // Fetching Data.
     useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all")
-            .then((response) => response.json())
-            .then((data) => {
-                setCountries(data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        // fetch("https://restcountries.com/v3.1/all")
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         setCountries(data);
+        //         setLoading(false);
+        //     })
+        //     .catch((error) => {
+        //         console.error(error);
+        //     });
     }, []);
 
     return (
-        <div className="bg-gray-100 dark:bg-gray-800 ">
+        <>
             {/* Container */}
             <div className=" mt-2">
                 {/* SearchBar */}
-                <div className=" mt-4  w-screen flex justify-center items-center bg-green-500">
+                <div className=" mt-4  flex justify-center items-center bg-green-500">
                     <input
                         type="text"
-                        className="bg-gray-600 dark:bg-gray-900 p-3 outline-none rounded-xl w-1/4"
+                        className="bg-gray-600 dark:bg-gray-900  outline-none rounded-xl "
                         placeholder="Search For A Country..."
                         onChange={(event) => setSearch(event.target.value)}
                     />
@@ -44,20 +45,15 @@ function Home() {
                     <>
                         {filterCountries.map((country, index) => {
                             return (
-                                <div key={index} className="bg-red-400 m-1">
-                                    <p>{country.name.official}</p>
-                                    <p>{country.capital}</p>
-
-                                    <p>{country.region}</p>
-                                    <p>{country.name.official}</p>
-                                    <img src={country.flags.png} alt="" />
-                                </div>
+                                <Link key={index}>
+                                    <CountryCard country={country} />
+                                </Link>
                             );
                         })}
                     </>
                 )}
             </div>
-        </div>
+        </>
     );
 }
 
