912a9144cde7a35339d509d1483fdb5f6d343756
EhsaN Karimi <ehsan.karimmi@gmail.com>
2023-06-24 18:43:36 +0330

    Add loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CgSpinner } from "react-icons/cg";
+
+function Loading() {
+    return <CgSpinner className="animate-spin h-12 w-12 rounded-full text-gray-950" />;
+}
+
+export default Loading;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CountryCard from "../components/CountryCard";
+import Loading from "../components/Loading";
 
 function Home() {
     const [countries, setCountries] = useState([]);
@@ -14,46 +15,44 @@ function Home() {
 
     // Fetching Data.
     useEffect(() => {
-        // fetch("https://restcountries.com/v3.1/all")
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         setCountries(data);
-        //         setLoading(false);
-        //     })
-        //     .catch((error) => {
-        //         console.error(error);
-        //     });
+        fetch("https://restcountries.com/v3.1/all")
+            .then((response) => response.json())
+            .then((data) => {
+                setCountries(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }, []);
 
     return (
-        <>
-            {/* Container */}
-            <div className=" mt-2">
-                {/* SearchBar */}
-                <div className=" mt-4  flex justify-center items-center bg-green-500">
-                    <input
-                        type="text"
-                        className="bg-gray-600 dark:bg-gray-900  outline-none rounded-xl "
-                        placeholder="Search For A Country..."
-                        onChange={(event) => setSearch(event.target.value)}
-                    />
-                </div>
-                {/* Show Country Or Loading During Fetch Data */}
-                {loading ? (
-                    <div>loading...</div>
-                ) : (
-                    <>
-                        {filterCountries.map((country, index) => {
-                            return (
-                                <Link key={index}>
-                                    <CountryCard country={country} />
-                                </Link>
-                            );
-                        })}
-                    </>
-                )}
-            </div>
-        </>
+        // Container.
+        <div className={loading ? `mt-2 h-[90%] flex justify-center items-center` : `mt-2`}>
+            {/* Show Country Or Loading During Fetch Data */}
+            {loading ? (
+                <Loading />
+            ) : (
+                <>
+                    {/* SearchBar */}
+                    <div className=" mt-4  flex justify-center items-center bg-green-500">
+                        <input
+                            type="text"
+                            className="bg-gray-600 dark:bg-gray-900  outline-none rounded-xl "
+                            placeholder="Search For A Country..."
+                            onChange={(event) => setSearch(event.target.value)}
+                        />
+                    </div>
+                    {filterCountries.map((country, index) => {
+                        return (
+                            <Link key={index}>
+                                <CountryCard country={country} />
+                            </Link>
+                        );
+                    })}
+                </>
+            )}
+        </div>
     );
 }
 
