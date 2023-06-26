18ec113af92e9482e7fa30166c8ef4828b1b3b83
EhsaN Karimi <ehsan.karimmi@gmail.com>
2023-06-26 18:44:42 +0330

    Add HashRouter From react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 
@@ -7,13 +7,13 @@ import Navbar from "./components/Navbar";
 function App() {
     return (
         <main className="bg-gray-100 dark:bg-gray-800 p-2">
-            <BrowserRouter>
+            <HashRouter>
                 <Navbar />
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                     <Route path="details" element={<Details />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </main>
     );
 }
