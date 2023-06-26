6b2e19638a07832ee4aa5b63b7c723c2d71fd1b5
EhsaN Karimi <ehsan.karimmi@gmail.com>
2023-06-26 18:32:39 +0330

    Fix Base URL bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
-import Page404 from "./pages/Page404";
+
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,9 +10,8 @@ function App() {
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route exact path="/React-Country-Search/" element={<Home />} />
+                    <Route exact path="/" element={<Home />} />
                     <Route path="details" element={<Details />} />
-                    <Route path="*" element={<Page404 />} />
                 </Routes>
             </BrowserRouter>
         </main>
